108c89937a09e59299c12527e85a2ee34154a740
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-11-14 08:30:47 +0100

    added skills icons and removed projects component from app component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import './Projects.sass';
 import ticTacToe from '../../assets/tic-tac-toe.png';
+import ProjectCard from './ProjectCard';
 
 export default function Projects() {
 	return (
 		<section id="projects" className="projects">
-			<h1 className="hero__title">Projects</h1>
-			<div className="hero__styles-projects-container">
-				<a
-					href="https://github.com/rts-cmk/tic-tac-toe-ReedorReed"
-					target="_blank">
-					<img className="hover" src={ticTacToe} alt="ticTacToe image" />
-					<h3>Tic Tac Toe</h3>
-					<p>Fun little game</p>
-				</a>
+			<h1 className="sectionTitle">Projects</h1>
+			<div className="projects__projects-container">
+				<ProjectCard
+					src={ticTacToe}
+					link={'https://github.com/rts-cmk/tic-tac-toe-ReedorReed'}
+					title="Tic Tac Toe"
+					subTitle="Fun little game"
+				/>
+				<ProjectCard
+					src={ticTacToe}
+					link={'https://github.com/rts-cmk/tic-tac-toe-ReedorReed'}
+					title="Tic Tac Toe"
+					subTitle="Fun little game"
+				/>
 			</div>
 		</section>
 	);
